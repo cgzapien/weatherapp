92a743830b7107d89276d84a2703af18c6c5f47f
cgzapien <65615888+cgzapien@users.noreply.github.com>
2022-01-27 18:46:33 -0700

    added 7 day forecast and working on showing data

--- src/App.js
+++ src/App.js
@@ -4,46 +4,42 @@ import Weather from "./Weather"
 import "./app.css"
 import { Box } from "@mui/system";
 import { Button } from "@mui/material";
-import { styled } from '@mui/material/styles';
 export default function App(){
   const [lat, setLat] = useState([])
   const [long, setLong] = useState([])
   const [data, setData] = useState([])
-  
+  const [dataLoaded, setDataLoaded] = useState(false)
   function handleLocation(){
     navigator.geolocation.getCurrentPosition((position) => {
       setLat(position.coords.latitude)
       setLong(position.coords.longitude)
     })
-    
+    setDataLoaded(true)
   }
   
   useEffect(()=> {      
       axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&appid=627a4c28ee22bae543d7ff8be2663f20`)
         .then(res => setData(() => [res.data]))
         .catch(err => console.log(err))
+        
   }, [lat, long])
-  const CustomBtn = styled(Button)({
-    backgroundColor: "#0063cc",
-    border: "1px solid",
-    fontSize: 16,
-  })
+  
   return (
     
       <Box className="appBox">
+        <br/>
+        <div className="btnContainer">
+          <Button
+          onClick={handleLocation}
+          variant="contained"
+          >My Location</Button>
+        </div>
         {data[0] !== undefined ? 
         <Weather weatherData={data} />
         :
         <></>
         }
         <br/>
-        <Button
-        className="locationBtn"
-        type="submit"
-        onClick={handleLocation}
-        variant="contained"
-        
-        >My Location</Button>
       </Box>
 
     
--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,10 @@
 import { Box, Card, Paper, Typography } from "@mui/material";
-import axios from "axios";
-import React, {useState} from "react";
+import React from "react";
 export default function Weather({weatherData}){
   console.log('weatherData: ', weatherData);
   const sunrise = new Date(weatherData[0].current.sunrise * 1000).toLocaleTimeString("en-US")
   const sunset = new Date(weatherData[0].current.sunset * 1000).toLocaleTimeString("en-US")
-  
+ 
   return (
    <Box className="weatherBox">
     
@@ -13,12 +12,34 @@ export default function Weather({weatherData}){
         <img alt="weathericon" src={`https://openweathermap.org/img/wn/${weatherData[0].current.weather[0].icon}@2x.png`}/>
         <p>{weatherData[0].current.weather[0].description}</p>
       </div>
-      <Typography>City: {weatherData[0].timezone.slice(8)}</Typography>
-      <Typography>Current Temp: {Math.floor(weatherData[0].current.temp)}&#xb0;F</Typography>
-      <Typography>Feels like: {Math.floor(weatherData[0].current.feels_like)}&#xb0;F</Typography>
-      <Typography>Sunrise: {sunrise}</Typography>
-      <Typography>Sunset: {sunset}</Typography>
-    
+      <div>
+        <Typography>City: {weatherData[0].timezone.slice(8)}</Typography>
+        <Typography>Current Temp: {Math.floor(weatherData[0].current.temp)}&#xb0;F</Typography>
+        {/* <Typography>Feels like: {Math.floor(weatherData[0].current.feels_like)}&#xb0;F</Typography> */}
+        <Typography>Sunrise: {sunrise}</Typography>
+        <Typography>Sunset: {sunset}</Typography>
+      </div>
+      <div className="forecast">
+        {weatherData[0].daily.map(weather => {
+          const date = new Date(weather.dt * 1000)
+          const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+          const day = weekday[date.getDay()]
+          console.log()
+          return (
+            <div>
+              <Typography>{day}</Typography>
+              <Typography>{weather.weather.map(sky => {
+                return (
+                  <>
+                    <img alt="weathericon" src={`https://openweathermap.org/img/wn/${sky.icon}@2x.png`}/>
+                    <Typography>{sky.main}</Typography>
+                  </>
+                )
+              })}</Typography>
+            </div>
+          )
+        })}
+      </div>
    </Box>
   )
 }
