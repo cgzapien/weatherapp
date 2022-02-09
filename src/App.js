5ad5ae5897ca499cb7ccf2ffbeeb956ebab99042
cgzapien <65615888+cgzapien@users.noreply.github.com>
2022-02-09 14:24:33 -0700

    refactored code to accept zip code instead

--- src/App.js
+++ src/App.js
@@ -3,63 +3,67 @@ import axios from "axios";
 import Weather from "./Weather"
 import "./app.css"
 import { Box } from "@mui/system";
-import { Button, Typography } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 export default function App(){
-  const [lat, setLat] = useState([])
-  const [long, setLong] = useState([])
   const [data, setData] = useState([])
-  const [dataLoaded, setDataLoaded] = useState(false)
-  function handleLocation(){
-    // navigator.geolocation.getCurrentPosition((position) => {
-    //   setLat(position.coords.latitude)
-    //   setLong(position.coords.longitude)
-    // })
-    setDataLoaded(true)
-    var options = {
-      enableHighAccuracy: true,
-      maximumAge: 0
-    };
+  const [zipCode, setZipCode] = useState({
+    zipcode: ""
+  })
+  const [coords, setCoords] = useState({
+    lat: "",
+    long: ""
+  })
+  const handleZipCodeChange = (e) => {
+    const { name, value } = e.target
+    setZipCode(prevState => ({
+      ...prevState, [name]:value
+    }))
+  };
+  const handleZipCode = (e) => {
+    e.preventDefault()
+    axios.get(`http://api.weatherapi.com/v1/forecast.json?key=0b5f9ec4960e4fcd9fe184625221901&q=${zipCode.zipcode}&days=7&aqi=no&alerts=no`)
+    .then(res => {
+      setData(() => [res.data])
     
-    function success(pos) {
-      var crd = pos.coords;
-      setLat(crd.latitude)
-      setLong(crd.longitude)
-      // console.log('Your current position is:');
-      // console.log(`Latitude : ${crd.latitude}`);
-      // console.log(`Longitude: ${crd.longitude}`);
-      // console.log(`More or less ${crd.accuracy} meters.`);
-    }
-    
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-    
-    navigator.geolocation.getCurrentPosition(success, error, options);
+    })
+    .catch(err => console.log(err))
   }
-  
-  useEffect(()=> {      
-      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&appid=627a4c28ee22bae543d7ff8be2663f20`)
-        .then(res => setData(() => [res.data]))
-        .catch(err => console.log(err))
-        
-  }, [lat, long])
-  
+//   useEffect(()=> {      
+//     axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.long}&units=imperial&appid=627a4c28ee22bae543d7ff8be2663f20`)
+//       .then(res => setData(() => [res.data]))
+//       .catch(err => console.log(err))
+      
+// }, [coords]) 
   return (
     
       <Box className="appBox">
-        <br/>
-        <div className="btnContainer">
-          <Button
-          onClick={handleLocation}
-          variant="contained"
-          >My Location</Button>
-        </div>
         <br/>
         {data[0] !== undefined ? 
         <Weather weatherData={data} />
         :
         <>
-        <Typography variant="body1" style={{textAlign: "center"}}>&#8593; Click the button to find your location &#8593;</Typography>
+        <div className="locationContainer" >
+          <br/>
+          <Box component="form" onSubmit={handleZipCode} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+            <TextField
+            onChange={handleZipCodeChange}
+            placeholder="Enter Zip Code"
+            name="zipcode"
+            value={zipCode.zipcode}
+            style={{WebkitAppearance: "none", margin: 0, textAlign: "center"}}
+            inputProps={{
+              maxLength: 5
+            }}
+            />
+            <br/>
+            <Button
+            variant="contained"
+            type="submit"
+            >Enter Zip Code</Button>
+
+          </Box>
+        <br/>
+        </div>
         </>
         }
         <br/>
--- src/Weather.js
+++ src/Weather.js
@@ -1,42 +1,38 @@
 import { Box, Card, Paper, Typography } from "@mui/material";
 import React from "react";
 export default function Weather({weatherData}){
-  const sunrise = new Date(weatherData[0].current.sunrise * 1000).toLocaleTimeString("en-US")
-  const sunset = new Date(weatherData[0].current.sunset * 1000).toLocaleTimeString("en-US")
+  console.log('weatherData: ', weatherData);
+  // const sunrise = new Date(weatherData[0].forecastday[0].sunrise * 1000).toLocaleTimeString("en-US")
+  // console.log('sunrise: ', sunrise);
+  // const sunset = new Date(weatherData[0].city.sunset * 1000).toLocaleTimeString("en-US")
   return (
    <Box className="weatherBox">
     
       <div>
-        <Typography variant="h3">{weatherData[0].timezone.slice(8)}</Typography>
-        <Typography variant="h4">{Math.floor(weatherData[0].current.temp)}&#xb0;F</Typography>
-        <img alt="weathericon" src={`https://openweathermap.org/img/wn/${weatherData[0].current.weather[0].icon}@2x.png`}/>
-        {/* <p>{weatherData[0].current.weather[0].description}</p> */}
+        <Typography variant="h3">{weatherData[0].location.name}</Typography>
+        <Typography variant="h4">{Math.floor(weatherData[0].current.temp_f)}&#xb0;F</Typography>
+        <img alt="weathericon" src={`${weatherData[0].current.condition.icon}`}/>
+        <p>{weatherData[0].current.condition.text}</p>
       </div>
       <div className="currentWeather">
-        <Typography variant="body2">Sunrise: {sunrise}</Typography>
-        <Typography variant="body2">Sunset: {sunset}</Typography>
-        <Typography variant="body2">Humidity: {weatherData[0].current.humidity} &#37;</Typography>
-        <Typography variant="body2">Feels like: {Math.floor(weatherData[0].current.feels_like)}&#xb0;F</Typography>
+        <Typography variant="body2">Sunrise: <br/>{weatherData[0].forecast.forecastday[0].astro.sunrise}</Typography>
+        <Typography variant="body2">Sunset: <br/>{weatherData[0].forecast.forecastday[0].astro.sunset}</Typography>
+        <Typography variant="body2">Humidity: <br/>{weatherData[0].current.humidity} &#37;</Typography>
+        <Typography variant="body2">Feels like: <br/>{Math.floor(weatherData[0].current.feelslike_f)}&#xb0;F</Typography>
       </div>
       <br/>
       <div className="forecast">
-        {weatherData[0].daily.map(weather => {
-          const date = new Date(weather.dt * 1000)
+        {weatherData[0].forecast.forecastday.map(weather => {
+          const date = new Date(weather.date_epoch * 1000)
           const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
           const dayNumber = date.getDate()
           const day = weekday[date.getDay()]
           return (
-            <div>
-              <Typography key={day}>{day} {dayNumber}</Typography>
-              <Typography>{Math.floor(weather.temp.day)}&#xb0;F</Typography>
-              {weather.weather.map(sky => {
-                return (
-                  <>
-                    <img key={sky.main} alt="weathericon" src={`https://openweathermap.org/img/wn/${sky.icon}@2x.png`}/>
-                    <Typography >{sky.main}</Typography>
-                  </>
-                )
-              })}
+            <div key={day} >
+              <Typography >{day}<br/>{weather.date.slice(5)}</Typography>
+              <img alt="weathericon" src={`${weather.day.condition.icon}`}/>
+              <Typography>{Math.floor(weather.day.avgtemp_f)}&#xb0;F</Typography>
+              <Typography >{weather.day.condition.text}</Typography>
             </div>
           )
         })}
